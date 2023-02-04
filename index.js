012f56f040ce4100af19d8e9112021da8e4b84b9
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2023-02-04 18:57:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('node:fs')
+var all = []
+var last = 0
+const { SuperfaceClient } = require("@superfaceai/one-sdk");
+const sdk = new SuperfaceClient();
+async function sleep(t) { return new Promise(resolve => setTimeout(() => resolve(), t)) }
+
+(async () => {
+  for (let index = 0; index < 172800; index++) {
+    await sleep(500)
+    var money = await new Promise(async resolve => {
+      const profile = await sdk.getProfile('crypto/exchange-rate@1.0.1');
+      const result = await profile.getUseCase("GetExchangeRate").perform({
+        from: "BTC",
+        to: "USDT",
+      }, {
+        provider: 'binance'
+      });
+      resolve(result.unwrap())
+    })
+    var price = money.rate
+    if (price == last) console.log("pareil", price)
+    else {
+      last = price
+      all.push(price)
+      fs.writeFileSync("./BTC.json", `[${all.join(",")}]`)
+    }
+  }
+})();
