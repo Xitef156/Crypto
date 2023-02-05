4b2f0c9bca2afa3d5486793863492dc807173424
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2023-02-05 20:47:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,44 +3,7 @@ var all = []
 var last = 0
 const { SuperfaceClient } = require("@superfaceai/one-sdk");
 const sdk = new SuperfaceClient();
-async function sleep(t) { return new Promise(resolve => setTimeout(() => resolve(), t)) }
-const http = require('http')
-
-const server = http.createServer(function(request, response) {
-  console.dir(request.param)
-
-  if (request.method == 'POST') {
-    console.log('POST')
-    var body = ''
-    request.on('data', function(data) {
-      body += data
-      console.log('Partial body: ' + body)
-    })
-    request.on('end', function() {
-      console.log('Body: ' + body)
-      response.writeHead(200, {'Content-Type': 'text/html'})
-      response.end('post received')
-    })
-  } else {
-    console.log('GET')
-    var html = `
-            <html>
-                <body>
-                    <form method="post" action="http://localhost:3000">Name: 
-                        <input type="text" name="name" />
-                        <input type="submit" value="Submit" />
-                    </form>
-                </body>
-            </html>`
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    response.end(html)
-  }
-})
-
-const port = 3000
-const host = 'localhost'
-server.listen(port, host)
-console.log(`Listening at http://${host}:${port}`);
+async function sleep(t) { return new Promise(resolve => setTimeout(() => resolve(), t)) };
 
 (async () => {
   for (let index = 0; index < 10; index++) {
