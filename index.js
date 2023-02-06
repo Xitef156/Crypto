d1be60c0acb14f3f9aae2962c6dd86ff1927262d
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2023-02-06 11:47:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var request = require('request')
-const { SuperfaceClient } = require("@superfaceai/one-sdk");
 const sdk = new SuperfaceClient();
 async function sleep(t) { return new Promise(resolve => setTimeout(() => resolve(), t)) };
 var err;
@@ -7,17 +6,21 @@ var err;
 (async () => {
   do {
     await sleep(500)
-    const profile = await sdk.getProfile('crypto/exchange-rate@1.0.1');
-    const result = await profile.getUseCase("GetExchangeRate").perform({
-      from: "BTC",
-      to: "USDT",
-    }, {
-      provider: 'binance'
-    });
-    const money = result.unwrap()
+    var money = 0
+    request({
+      method: 'get',
+      url: "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+     }, (error, res ,body => {
+      if(error){
+        console.log('error', error)
+        err = 500
+      }
+      money = JSON.parse(body).price
+    })
+    console.log(money)
     var options = {
       method: 'post',
-      body: {price: money.rate}, // Javascript object
+      body: {price: money}, // Javascript object
       json: true, // Use,If you are sending JSON data
       url: "https://Crypto.xitef156.repl.co"
     }
