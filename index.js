3a3dce67070820127b9fc087bd2f81f216be8be3
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2023-02-06 09:40:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,27 +6,25 @@ async function sleep(t) { return new Promise(resolve => setTimeout(() => resolve
 (async () => {
   while (true) {
     await sleep(500)
-    var money = await new Promise(async resolve => {
-      const profile = await sdk.getProfile('crypto/exchange-rate@1.0.1');
-      const result = await profile.getUseCase("GetExchangeRate").perform({
-        from: "BTC",
-        to: "USDT",
-      }, {
-        provider: 'binance'
-      });
-      resolve(result.unwrap())
-    })
-      var options = {
-        method: 'post',
-        body: {price: money.rate}, // Javascript object
-        json: true, // Use,If you are sending JSON data
-        url: "https://Crypto.xitef156.repl.co"
+    const profile = await sdk.getProfile('crypto/exchange-rate@1.0.1');
+    const result = await profile.getUseCase("GetExchangeRate").perform({
+      from: "BTC",
+      to: "USDT",
+    }, {
+      provider: 'binance'
+    });
+    const money = result.unwrap()
+    var options = {
+      method: 'post',
+      body: {price: money.rate}, // Javascript object
+      json: true, // Use,If you are sending JSON data
+      url: "https://Crypto.xitef156.repl.co"
+    }
+    request(options, function (err) {
+      if (err) {
+        console.log('Error :', err)
+        break;
       }
-      request(options, function (err) {
-        if (err) {
-          console.log('Error :', err)
-          return
-        }
-      });
+    });
   }
 })();
