ab3bdf287d9d9fd4da7d0a5583717ff4fa703f44
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2023-02-06 14:54:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ async function sleep(t) { return new Promise(resolve => setTimeout(resolve, t)) 
     try{
       await sleep(500)
       var price = (await axios.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")).data.price
-      //if(`${price}.000`.slice(0,9) == NaN)
-      console.log(price)
-      axios.post("https://Crypto.xitef156.repl.co", {price: `${price}.000`.slice(0,9)}).catch(async() => await sleep(500))
+      axios.post("https://Crypto.xitef156.repl.co", {price: Number(price)).catch(async() => await sleep(500))
       //console.log(price)
     } catch(e){
       console.error(e)
