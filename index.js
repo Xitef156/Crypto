14b9a3725d5493caf03bb6d04e612cef01acf1ff
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2023-02-06 12:16:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-var request = require('request')
-async function sleep(t) { return new Promise(resolve => setTimeout(() => resolve(), t)) };
-var err;
+const {Axios} = require('axios')
+async function sleep(t) { return new Promise(resolve => setTimeout(resolve, t)) };
 
 (async () => {
   do {
-    await sleep(500)
-    var money = 0
-    request({
-      method: 'get',
-      url: "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-     }, (error, res ,body) => {
-      if(error){
-        console.log('error', error)
-        err = 500
-      }
-    console.log(JSON.parse(body).price)
-      money = JSON.parse(body).price
-    })
-    var options = {
-      method: 'post',
-      body: {price: money}, // Javascript object
-      json: true, // Use,If you are sending JSON data
-      url: "https://Crypto.xitef156.repl.co"
+    try{
+      await sleep(500)
+      var money = 0
+      var price = await new Axios().get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+      money = JSON.parse(price).price
+      console.log(money)
+      new Axios().post("https://Crypto.xitef156.repl.co", {price: price})
+    } catch(e){
+      console.error(e)
     }
-    request(options, error => {
-      if (error) {
-        console.log('Error :', error)
-        err = error
-        return;
-      }
-    });
-  } while (err == undefined)
+  } while (true)
 })();
