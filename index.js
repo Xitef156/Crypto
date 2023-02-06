7ade971c8ac4c8406eda08f0e01012b3e2f1ea94
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2023-02-06 18:51:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const axios = require('axios').default
 async function sleep(t) { return new Promise(resolve => setTimeout(resolve, t)) };
+const express = require('express');
+const app = express();
+const port = 3000;
+var go = false
+
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+
+app.get('/go', (req, res) => {
+  go = true
+  res.send('go')
+})
+
+app.get('/stop', (req, res) => {
+  go = false
+  res.send('stop')
+})
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+});
 
 (async () => {
   do {
     try{
-      var price = (await axios.get(process.env["Crypto"])).data.price
-      axios.post(process.env["URL"], {price: Number(price)}).catch(async() => await sleep(500))
-      //console.log(price)
+      if(go){
+        var price = (await axios.get(process.env["Crypto"])).data.price
+        axios.post(process.env["URL"], {price: Number(price)}).catch(async() => await sleep(500))
+        //console.log(price)
+      }
     } catch(e){
       console.error(e)
     }
