fb99f3eb237b92a17cc8f0ffa460e1f172c176ed
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2023-02-06 17:33:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ async function sleep(t) { return new Promise(resolve => setTimeout(resolve, t)) 
 (async () => {
   do {
     try{
-      var price = (await axios.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")).data.price
-      axios.post("https://Crypto.xitef156.repl.co", {price: Number(price)}).catch(async() => await sleep(500))
+      var price = (await axios.get(process.env["Crypto"])).data.price
+      axios.post(process.env["URL"], {price: Number(price)}).catch(async() => await sleep(500))
       //console.log(price)
     } catch(e){
       console.error(e)
