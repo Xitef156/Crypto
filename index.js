bea84580de073a2397557a53b84d450099052c1c
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2023-02-08 14:56:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
+  const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+})
+/*
+
 const axios = require('axios').default
 async function sleep(t) { return new Promise(resolve => setTimeout(resolve, t)) };
 (async () => {
